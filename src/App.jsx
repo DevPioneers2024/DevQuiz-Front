68a62d466f8352f1df1074f8e6137184500ae743
Robert Medina <csoperativo@gmail.com>
2024-04-16 09:04:36 -1000

    Add component intruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Intruction } from './components/instruction/Intruction'
 import SliderUI from './components/Slider/SliderUI'
 
 function App() {
   return (
     <>
       <SliderUI />
+      <Intruction />
     </>
   )
 }
