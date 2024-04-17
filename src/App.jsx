4d1636b8b5a3ef3ac24f918dd9eaef9a73379565
lilasca <lilasca1998@hotmail.com>
2024-04-17 12:30:31 -0500

    componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+
 import './App.sass'
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <div className="header">
+      <div className="logo-container">
+        <img src="https://th.bing.com/th/id/OIF.KJieHVy0gNCxNHR6gY226w?w=184&h=184&c=7&r=0&o=5&pid=1.7" alt="Logo" className="logo" />
+      </div>
+      <div className="auth-buttons">
+        <button className="login">Iniciar sesión</button>
+        <button className="register">Registrarse</button>
+      </div>
+    </div>
     </>
   )
 }
