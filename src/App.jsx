090906880b249535a726e3b113e0d62e54f66bad
Fidel Choque <devcrash03@gmail.com>
2024-04-18 19:30:10 +0200

    add utils button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.sass'
+import Button from './utils/Button'
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
+      <Button />
     </>
   )
 }
---
+++ src/utils/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Button(title) {
+  return (
+    <button>{title}</button>
+  )
+}
+
+export default Button
