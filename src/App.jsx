5573a473ca20d0235edcb233634935a668cd05c2
lilasca <lilasca1998@hotmail.com>
2024-04-20 10:58:12 -0500

    fix  component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
+import Navbar from "./components/Navbar";
 
-import './App.sass'
-
-function App() {
+function App () {
   return (
-    <>
-      <div className="header">
-      <div className="logo-container">
-        <img src="https://th.bing.com/th/id/OIF.KJieHVy0gNCxNHR6gY226w?w=184&h=184&c=7&r=0&o=5&pid=1.7" alt="Logo" className="logo" />
-      </div>
-      <div className="auth-buttons">
-        <button className="login">Iniciar sesión</button>
-        <button className="register">Registrarse</button>
-      </div>
-    </div>
-    </>
+    <Navbar />
+      
+    
   )
 }
 
