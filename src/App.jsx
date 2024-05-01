2fd8a4a5ce794dc06550932235d016928a4301b9
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2024-05-01 04:31:47 +0200

    route was added to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ import {
 }from 'react-router-dom';
 
 import { Home } from './pages/Home'
+import { Gameroom } from './pages/Gameroom';
+import { WaitRooms } from './pages/WaitRooms';
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Rutas>
+        <Ruta path="/" element = {<Home />} />
+        <Ruta path="/Gameroom" element = {<Gameroom />} />
+        <Ruta path="/WaitRooms" element = {<WaitRooms />} />
+      </Rutas>
+    </Router>
+    
+  )
 }
 
 export default App
