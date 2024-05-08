467e93e790bf1e4ff2f76675f838279748d2398d
Fidel Choque <devcrash03@gmail.com>
2024-05-08 18:52:55 +0200

    removing repeated lines and adding components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Routes as Rutas,
-  Route as Ruta 
-}from 'react-router-dom';
-
 import { Home } from './pages/Home'
 import { Gameroom } from './pages/Gameroom';
 import { WaitRooms } from './pages/WaitRooms';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { WaitRooms } from './pages/WaitRooms'
 
 function App() {
   return (
@@ -16,6 +9,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/room/:id' element={<WaitRooms />} />
+        <Route path='/Gameroon' element={<Gameroom />} />
       </Routes>
     </Router>
   )
