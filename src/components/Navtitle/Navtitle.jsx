2a33ed079cbc9825680105c0e9e597aa2a6c5300
Fidel Choque <82333258+fidelChoqueDev@users.noreply.github.com>
2024-05-13 19:17:37 +0200

    modificaciones para respomsive

--- src/components/Navtitle/Navtitle.jsx
+++ src/components/Navtitle/Navtitle.jsx
@@ -1,9 +1,8 @@
 import style from '../../sass/Gameroom.module.sass'
-import Imagen from '../../../../../capturas/Rocky.png'
 const Navtitle = () => {
   return (
     <div className={style.navtitle}>
-        <img src={Imagen} alt="" />
+        <img src="https://images.unsplash.com/photo-1613254026301-71fd1a7fd020?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM4fHxzdGFyJTIwd2Fyc3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
         <span className={style.navtitle__logo}>Rocky</span>
     </div>
   )
