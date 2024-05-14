8327357a7a3a3c643f2061fb0d23102bc6e2a95d
Fidel Choque <devcrash03@gmail.com>
2024-05-14 19:46:53 +0200

    Modi componente preguntas a la version del compañero lilasca

--- src/components/answer/Preguntas.jsx
+++ src/components/answer/Preguntas.jsx
@@ -6,27 +6,27 @@ import 'react';
 function Preguntas() {
     return (
         <section className={style.useranswer}>
-            <main>
-                <article className="Preguntas-question">
-                    <h2 className="Preguntas-questionText">¿Esta sera donde entre la pregunta...?</h2>
-                    <form className="Preguntas-answers">
+            <main className={style.useranswer__main}>
+                <article className={style.useranswer__main__question}>
+                    <h2 className={style.useranswer__main__questionText}>¿Esta sera donde entre la pregunta...?</h2>
+                    <form className={style.useranswer__main__answers}>
 
-                        <div className="Preguntas-answer">
+                        <div className={style.useranswer__main__answers__answer}>
                             <input type="radio" name="answers" id="opcion1" value="opcion1"/>
                             <label htmlFor='opcion1'>Esta será la primera respuesta</label>
                         </div>
 
-                        <div className="Preguntas-answer">
+                        <div className={style.useranswer__main__answers__answer}>
                             <input type="radio" name="answers" id='opcion2' value="opcion2"/>
                             <label htmlFor='opcion2'>Esta será la segunda respuesta</label>
                         </div>
 
-                        <div className="Preguntas-answer">
+                        <div className={style.useranswer__main__answers__answer}>
                             <input type="radio" name="answers" id='opcion3' value="opcion3"/>
                             <label htmlFor='opcion3'>Esta será la tercera respuesta</label>
                         </div>
                     
-                        <div className="Preguntas-answer">
+                        <div className={style.useranswer__main__answers__answer}>
                             <input type="radio" name="answers" id='opcion4'value="opcion4"/>
                             <label htmlFor='opcion4'>Esta será la cuarta respuesta</label>
                         </div>
